51a707367f0f23d3362e78ae66a689e39224708c
Yichun Qian <76653849+EmilyQian2001@users.noreply.github.com>
2023-08-09 16:31:06 +0800

    Create Post.js

---
+++ components/Post.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Post = () => {
+  return (
+    <div className="flex flex-col">
+      <div className="bg-white mt-6 rounded-md p-4">
+        <div className="flex items-center">post</div>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
