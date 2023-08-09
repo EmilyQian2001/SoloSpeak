9ba8fb021bad70ae69e891dbbdb93498be0e8b4b
Yichun Qian <76653849+EmilyQian2001@users.noreply.github.com>
2023-08-09 16:34:10 +0800

    Update Feed.js

--- components/Feed.js
+++ components/Feed.js
@@ -5,7 +5,6 @@ import Posts from "./Posts";
 const Feed = () => {
   return (
     <div className="flex-grow h-screen pt-6 mr-6 overflow-y-auto">
-      feed
       <div className="mx-auto max-w-md md:max-w-xl lg:max-w-2xl">
         {/* Create Post Box */}
         <CreatePost />
