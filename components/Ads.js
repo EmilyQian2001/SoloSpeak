4ce3e4b00d16bb3c43e3d74ad17bcbc2ee5813f0
EmilyQian2001 <1044824871@qq.com>
2023-08-10 15:32:02 +0800

    update ads image size

--- components/Ads.js
+++ components/Ads.js
@@ -7,8 +7,8 @@ const Ads = ({ src, url }) => {
       <div className="flex items-center space-x-4 py-2 px-2 hover:bg-gray-200 rounded-md">
         <Image
           src={src}
-          height={160}
-          width={160}
+          height={140}
+          width={140}
           className="rounded-md cursor-pointer"
         />
         <p className="text-sm">{new URL(url).hostname}</p>
