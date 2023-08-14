2534a31254eb51ed5278812c2b9dd90e776c4d0e
EmilyQian2001 <1044824871@qq.com>
2023-08-14 22:48:27 +0800

    https

--- components/Posts.js
+++ components/Posts.js
@@ -5,7 +5,7 @@ import { addAllPost, selectPost } from "@/redux/features/postSlice";
 import axios from "axios";
 
 const Posts = () => {
-  const FACEBOOK_CLONE_ENDPOINT = "https://localhost:8080/api/v1/post";
+  const FACEBOOK_CLONE_ENDPOINT = "https://localhost:8443/api/v1/post";
   const dispatch = useDispatch();
   const posts = useSelector(selectPost);
   useEffect(() => {
